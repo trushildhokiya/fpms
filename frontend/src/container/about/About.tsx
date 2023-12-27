b7091692e50e3ab4ef68381efef4ed4b63ac830f
Trushil <95337251+trushildhokiya@users.noreply.github.com>
2023-12-27 10:49:24 +0530

    updated about page

--- frontend/src/container/about/About.tsx
+++ frontend/src/container/about/About.tsx
@@ -1,47 +1,150 @@
 import CommonNavbar from "@/components/navbar/CommonNavbar"
+import { Card, CardContent } from "@/components/ui/card"
+import { Linkedin, Github } from "lucide-react"
 
 const About = () => {
+
+  const teamData = [
+    {
+      id: 1,
+      name: 'Trushil Dhokiya',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas sed aut ipsam?',
+      linkedinURI: 'https://in.linkedin.com/in/trushil-dhokiya',
+      githubURI: 'https://www.github.com/trushildhokiya',
+    },
+    {
+      id: 2,
+      name: 'Hitanshu Gandhi',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas sed aut ipsam?',
+      linkedinURI: '',
+      githubURI: '',
+    },
+    {
+      id: 3,
+      name: 'Tavion Fernandes',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas sed aut ipsam?',
+      linkedinURI: '',
+      githubURI: '',
+    },
+    {
+      id: 4,
+      name: 'Ojas Golatkar',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas sed aut ipsam?',
+      linkedinURI: '',
+      githubURI: '',
+    },
+    {
+      id: 5,
+      name: 'Kushal Harsora',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas sed aut ipsam?',
+      linkedinURI: '',
+      githubURI: '',
+    },
+    {
+      id: 6,
+      name: 'Devam Dixit',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas sed aut ipsam?',
+      linkedinURI: '',
+      githubURI: '',
+    },
+  ]
+
   return (
     <div>
       <CommonNavbar />
-      <div className="about-us p-8">
-        <h1 className="text-3xl font-bold mb-4">About Faculty Profile Management System</h1>
-        <p className="text-gray-700 mb-6">
-          Welcome to the Faculty Profile Management System developed by the Computer Department at KJSIT. Our team has worked diligently to create an efficient and user-friendly platform tailored to meet the specific needs of our faculty members.
+      <div className="container font-Poppins">
+
+        {/* HEADING  */}
+        <h1 className="font-AzoSans font-bold text-3xl md:text-5xl tracking-wide my-6 text-red-800 ">
+          <span className="border-b-4 border-red-800">
+            ABOUT
+          </span>
+        </h1>
+
+        {/* STARTER TEXT  */}
+        <p className="text-red-800 text-xl font-OpenSans font-bold my-2 ">
+          Greetings from the Computer Department at KJSIT!
         </p>
 
-        <h2 className="text-2xl font-bold mb-2">Our Mission</h2>
-        <p className="text-gray-700 mb-6">
-          The primary mission of this system is to streamline the management of faculty profiles, providing a centralized hub for storing, updating, and accessing important information related to our esteemed educators. By leveraging modern web technologies, we aim to enhance efficiency and collaboration within the academic community.
-        </p>
+        <div className="leading-8 text-md md:text-justify">
+          <p className="my-3">
+            We're thrilled to introduce you to the Faculty Profile Management System (FPMS), a cutting-edge platform designed to revolutionize the way we manage and showcase the achievements of our esteemed faculty members. 🚀
+          </p>
 
-        <h2 className="text-2xl font-bold mb-2">Key Features</h2>
-        <ul className="list-disc list-inside text-gray-700 mb-6">
-          <li>Effortless Profile Management: Easily update and maintain personal and professional information.</li>
-          <li>Research Showcase: Highlight research publications, projects, and achievements.</li>
-          <li>Interactive User Interface: Enjoy a seamless and intuitive experience with our user-friendly interface.</li>
-          <li>Data Security: Your information is secure with robust authentication and authorization mechanisms.</li>
-          <li>Responsive Design: Access the system from any device, ensuring a consistent experience.</li>
-        </ul>
+          <p className="my-3">
+            At FPMS, we believe in fostering innovation, collaboration, and efficiency within our academic community. This system is crafted with precision to meet the unique needs of our faculty, providing a centralized hub for seamless profile management and information sharing.
+          </p>
+        </div>
 
-        <h2 className="text-2xl font-bold mb-2">Technology Stack</h2>
-        <p className="text-gray-700 mb-6">
-          The Faculty Profile Management System is built using TSX, a variant of TypeScript commonly used in conjunction with React for building user interfaces. This allows us to deliver a dynamic and responsive application that meets the standards of modern web development.
-        </p>
+        {/* IMAGE  */}
+        <div className="flex justify-center my-[5rem]">
+          <img src='https://picsum.photos/800/300' className="rounded-lg" alt='random-image' />
+        </div>
 
-        <h2 className="text-2xl font-bold mb-2">Feedback and Support</h2>
-        <p className="text-gray-700 mb-6">
-          We value your feedback! If you have any suggestions, encounter issues, or need assistance, please don't hesitate to reach out to our support team. Your input is crucial in helping us improve and refine the system.
-        </p>
+        {/* MEET THE DEVELOPERS  */}
+        <h2 className="font-OpenSans text-2xl text-red-800 my-6 font-bold ">
+          OUR TEAM
+        </h2>
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
 
-        <h2 className="text-2xl font-bold mb-2">Thank You</h2>
-        <p className="text-gray-700 mb-6">
-          We express our gratitude to the faculty members, administrators, and everyone involved in the development and deployment of the Faculty Profile Management System. Your collaboration and support have been instrumental in making this project a reality.
-        </p>
+          {
+            teamData.map((member) => {
+              return (
 
-        <p className="text-gray-700">
-          Thank you for using the Faculty Profile Management System at KJSIT. We are committed to continuous improvement and providing a platform that enhances the overall academic experience for our faculty members.
-        </p>
+                <Card key={member.id}>
+                  <CardContent className="p-0 m-0">
+                    <div className="grid grid-cols-3">
+                      <div className="col-span-1">
+                        <img src='https://picsum.photos/400/600' className="rounded-lg" alt='random-image' />
+                      </div>
+                      <div className="col-span-2 mx-4 my-4">
+                        <h2 className="font-OpenSans text-lg text-gray-700 font-bold">
+                          {member.name}
+                        </h2>
+                        <p className="text-sm text-gray-600 leading-6 my-3">
+                          {member.description}
+                        </p>
+                        <div className="flex justify-center">
+                          <a href={member.linkedinURI} target="_blank" >
+                            <div className="bg-red-800 p-2 rounded-full text-white mx-3 my-3">
+                              <Linkedin />
+                            </div>
+                          </a>
+                          <a href={member.githubURI} target="_blank">
+                            <div className="bg-red-800 p-2 rounded-full text-white mx-3 my-3">
+                              <Github />
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+
+              )
+            })
+          }
+
+        </div>
+
+        {/* FEEDBACK */}
+        <h2 className="font-OpenSans text-2xl text-red-800 font-bold my-4 mt-[5rem]">
+          FEEDBACK
+        </h2>
+        <div className="mb-[5rem]">
+          <p className="leading-6 md:text-justify my-3">
+            At FPMS, your feedback is invaluable to us. We strive for excellence and continuous improvement, and your insights play a crucial role in shaping the future of our platform.
+          </p>
+          <p className="leading-6 md:text-justify my-3">
+
+            Whether you have suggestions, encounter challenges, or simply want to share your experience, we welcome your thoughts with open arms. Your feedback empowers us to refine and enhance the Faculty Profile Management System, ensuring it meets the evolving needs of our academic community.
+          </p>
+          <p className="leading-6 md:text-justify my-3">
+
+            Thank you for taking the time to contribute to the growth of FPMS. Together, let's build a platform that truly serves and elevates the academic experience for everyone.
+          </p>
+        </div>
+
       </div>
     </div>
   )
