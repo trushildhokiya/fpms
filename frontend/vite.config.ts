c9cf271db896988e56f0b9d36126eed26e3eddd8
Trushil <95337251+trushildhokiya@users.noreply.github.com>
2024-01-06 15:15:38 +0530

    injected pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,56 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from 'vite-plugin-pwa'
+
+
+const manifestForPlugIn = {
+  registerType: "prompt" as const,
+  includeAssests:['favicon.ico', "apple-touch-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"Faculty Profile Management System",
+    short_name:"fpms",
+    description:"Faculty Profile Management System",
+    icons:[{
+      src: '/android-chrome-192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'/android-chrome-512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color: "#991b1b",
+  background_color: "#ffffff",
+  display:"standalone" as const,
+  scope:'/',
+  start_url:"/",
+  orientation:'any' as const
+  }
+}
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),VitePWA(manifestForPlugIn)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 })
+
