775cf6e64805f3674d977433094548017235130c
Trushil <95337251+trushildhokiya@users.noreply.github.com>
2024-07-10 11:05:47 +0530

    reporting update III

--- frontend/src/container/views/admin/container/display/patent.tsx
+++ frontend/src/container/views/admin/container/display/patent.tsx
@@ -254,7 +254,7 @@ const PatentDisplay = (props: Props) => {
 
 
     const header = (
-        <div className="flex w-full justify-end gap-x-6 font-Poppins">
+        <div className="flex w-full justify-end gap-6 flex-wrap font-Poppins">
             <Button className='rounded-full bg-green-600' onClick={() => exportCSV(false)}>
                 <Table className='w-5 h-5 mr-2' /> Download
             </Button>
